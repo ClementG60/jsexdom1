80fe1ec7d48247848e580857534bdc8915bd08dc
ClementG60 <cle_guillaume@hotmail.fr>
2021-11-04 13:46:58 +0100

    ajout code

---
+++ ex1.js
@@ -0,0 +1,11 @@
+function verificationPassword(){
+    let p1 = document.getElementById("password1").value
+    let p2 = document.getElementById("password2").value
+    if (p1 === p2) {
+        document.getElementById("password1").style.borderColor = "green"
+        document.getElementById("password2").style.borderColor = "green"
+    } else {
+        document.getElementById("password1").style.borderColor = "red"
+        document.getElementById("password2").style.borderColor = "red"
+    }
+}
