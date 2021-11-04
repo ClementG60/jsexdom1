42b1ce65e58021b7082d45468df2a46ab4d95070
ClementG60 <cle_guillaume@hotmail.fr>
2021-11-04 13:48:33 +0100

    ajout code

--- ex1.js
+++ ex1.js
@@ -1,7 +1,7 @@
 function verificationPassword(){
     let p1 = document.getElementById("password1").value
     let p2 = document.getElementById("password2").value
-    if (p1 === p2) {
+    if (p1 == p2) {
         document.getElementById("password1").style.borderColor = "green"
         document.getElementById("password2").style.borderColor = "green"
     } else {
